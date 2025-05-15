e100f4b41feb8a91ff2f29a71a63d87dde64b1ef
Xiangplusss <126126325+Xiangplusss@users.noreply.github.com>
2025-05-15 21:27:28 +0800

    mod

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// nuxt.config.ts
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: true,
+  app: {
+    baseURL: '/warehouse/' // 仓库名
+  },
+  css: ["@/assets/css/main.css"],
+  modules: [
+    "@nuxt/image",
+    "nuxt-icon",
+    "@nuxtjs/tailwindcss", // Tailwind CSS
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  components: {
+    dirs: ["~/components", "~/components/Icons"],
+  },
+})
