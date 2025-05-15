21f7374940631ad093244aaa8a82efedc35bbd4c
Xiangplusss <126126325+Xiangplusss@users.noreply.github.com>
2025-05-15 21:11:26 +0800

    init

---
+++ pages/internal/dashboard/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <InternalLayout>
+        <Dashboard />
+    </InternalLayout>
+</template>
+
+<script setup lang="ts">
+    import InternalLayout from "@/layouts/internal.vue";
+    import Dashboard from "@/components/Dashboard.vue";
+</script>
---
+++ pages/internal/settings/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <InternalLayout>
+        <Setting />
+    </InternalLayout>
+</template>
+
+<script setup lang="ts">
+import InternalLayout from "~/layouts/internal.vue";
+import Setting from "~/components/Setting.vue";
+</script>
