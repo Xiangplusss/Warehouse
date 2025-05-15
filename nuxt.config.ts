827c3f2e24ac3a1c7b16fe55fa1287895706b734
Xiangplusss <126126325+Xiangplusss@users.noreply.github.com>
2025-05-15 21:32:44 +0800

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   app: {
-    baseURL: '/warehouse/' // 仓库名
+    baseURL: '/warehouse/', 
+    head: {
+      title: 'Warehouse Management System',
+      meta: [
+        { name: 'description', content: '智能仓库管理系统，提升效率，安全可靠。' },
+        { name: 'keywords', content: '仓库,管理系统,智能,物流' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+    }
   },
   css: ["@/assets/css/main.css"],
   modules: [
     "@nuxt/image",
     "nuxt-icon",
-    "@nuxtjs/tailwindcss", // Tailwind CSS
+    "@nuxtjs/tailwindcss", 
   ],
   postcss: {
     plugins: {
